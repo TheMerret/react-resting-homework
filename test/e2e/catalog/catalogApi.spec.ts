dfacdc6e97fe54f3345d4496ab227b497bd67b84
TheMerret <merret@bk.ru>
2023-07-02 22:22:41 +0300

    test bug 3

--- test/e2e/catalog/catalogApi.spec.ts
+++ test/e2e/catalog/catalogApi.spec.ts
@@ -11,3 +11,17 @@ test("cart should show entries from api", async ({ page }) => {
   const link = await page.locator("a.card-link").first().getAttribute("href");
   expect(link?.endsWith("/hw/store/catalog/0")).toBe(true);
 });
+
+test("cart should show different entries from api", async ({ page }) => {
+  await page.setViewportSize({ width: 1200, height: 644 });
+  await page.goto("./catalog/0");
+  const prevTitle = await(
+    await page.waitForSelector(".ProductDetails-Name")
+  ).textContent() as string;
+  await page.goto("./catalog/1");
+  const curTitle = await(
+    await page.waitForSelector(".ProductDetails-Name")
+  ).textContent() as string;
+  await expect(prevTitle).not.toBe(curTitle);
+});
+
